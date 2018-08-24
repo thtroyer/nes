630ab293b6ce74e0e987581e349d8d7be5db42f6
Tom Troyer <tom.troyer@gmail.com>
2018-08-23 23:58:01 -0400

    Replacing nesasm with ca65 version now that it is comparable

--- hello_world/hello_world.asm
+++ hello_world/hello_world.asm
@@ -1,38 +1,70 @@
-;;; USAGE ;;;
-; Compile with nesasm
-; $ nesasm hello_world.asm
-; Then run .nes file with an NES emulator
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; .ines header  
-  .inesprg 1
-  .ineschr 1
-  .inesmap 0
-  .inesmir 1
+.setcpu "6502"
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Variable declarations
-  .rsset $0000
-sprite_x .rs 1
-sprite_y .rs 1
-buttons .rs 1
-pointerLo .rs 1
-pointerHi .rs 1
+PPU_CTRL1 = $2000
+PPU_CTRL2 = $2001
+PPU_STATUS = $2002
+PPU_SPR_ADDR = $2003
+PPU_SPR_IO = $2004
+PPU_VRAM_ADDR1 = $2005
+PPU_VRAM_ADDR2 = $2006
+PPU_VRAM_IO = $2007
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Subroutine definitions
-  .bank 0
-  .org $C000
+APU_MODCTRL = $4010
+APU_SPR_DMA = $4014
 
+APU_PAD1 = $4016
+APU_PAD2 = $4017
+
+CRTL2_GRAYSCALE = %00000001
+CRTL2_DISABLE_LEFT_BG_CLIP = %00000010
+CRTL2_DISABLE_LEFT_SPR_CLIP = %00000100
+CRTL2_BACKGROUND_RENDER = %00001000
+CRTL2_SPRITE_RENDER = %00010000
+CRTL2_INTENSE_RED = %00100000
+CRTL2_INTENSE_GREEN = %01000000
+CRTL2_INTENSE_BLUE = %10000000
+
+CRTL1_NMI_ENABLED = %10000000
+CRTL1_SPRITES_8x16 = %00100000
+CRTL1_BG_PT_ADDR_1 = %00010000
+CRTL1_SP_PT_ADDR_1 = %00001000
+CRTL1_VRAM_INC = %00000100
+CRTL1_BASE_NAMETABLE_2000 = %00000000
+CRTL1_BASE_NAMETABLE_2400 = %00000001
+CRTL1_BASE_NAMETABLE_2800 = %00000010
+CRTL1_BASE_NAMETABLE_2C00 = %00000011
+
+.segment "HEADER"
+; the header!
+.byte "NES", $1a ; ines header
+.byte 1 ; PRG-ROM pages (16kb)
+.byte $01 ; CHR-ROM pages (8kb)
+
+
+; Zero page RAM
+.segment "ZEROPAGE"
+sprite_x: .res 1
+sprite_y: .res 1
+buttons: .res 1
+pointerHigh: .res 1
+pointerLow: .res 1
+
+
+; General RAM
+;.segment "BSS"
+
+
+.segment "CODE"
 
 ReadController:
   LDA #$01
-  STA $4016
+  STA APU_PAD1 
   LDA #$00
-  STA $4016
+  STA APU_PAD1 
   LDX #$08
 
 ReadControllerLoop:
-  LDA $4016
+  LDA APU_PAD1 
   ; use bit shifting to pull lowest bit from controller and put into buttons
   LSR A
   ROL buttons
@@ -71,9 +103,9 @@ CopySpritesToPpu:
   ;; copy sprites back to PPU as the PPU forgets them every cycle.
   ; write address $0200 to PPU for DMA sprite transfer
   LDA #$00
-  STA $2003
+  STA PPU_SPR_ADDR
   LDA #$02
-  STA $4014
+  STA APU_SPR_DMA 
   RTS
 
 HandleControllerInput:
@@ -99,37 +131,43 @@ HandleControllerInput:
 
   RTS
 
+vblank_cycle:
+  lda PPU_STATUS 
+  bpl vblank_cycle
+  rts
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Main program start
-RESET:
+Reset:
   SEI
   CLD
+  .repeat 3
+  jsr vblank_cycle
+  .endrep
+
   LDA #$80
   STA sprite_x
   STA sprite_y
-  LDA #$00
-  STA $0052
 
   ;; Palettes start at PPU address $3F00 and $3F10. To set this address, PPU address port $2006 is used.
-  ;; The port must be written twice, once for the high byte then for the low byte.
   ;;
   ;; This code tells the PPU to set its address to $3F10. Then the PPU data port at $2007 is ready to accept data. The first write will go to the
   ;; address you set ($3F10), then the PPU will automatically increment the address after each read or write.
 LoadPalettes:
   ;read PPU status to reset the high/low latch to high
-  LDA $2002
+  LDA PPU_STATUS 
   ;write 3F00 to set its address there
   LDA #$3F
-  STA $2006
+  STA PPU_VRAM_ADDR2 
   LDA #$00
-  STA $2006
+  STA PPU_VRAM_ADDR2 
 
   LDX #$00
 LoadPalettesLoop:
   ; loop through palettes and write to PPU ($2007)
   ; 32 bytes (0x20)
-  LDA palettes, x
-  STA $2007
+  LDA Palettes, x
+  STA PPU_VRAM_IO 
   INX
   CPX #$20
   BNE LoadPalettesLoop
@@ -137,186 +175,174 @@ LoadPalettesLoop:
 LoadSprites:
   LDX #$00
 LoadSpritesLoop:
-  LDA sprites, x
+  LDA Sprites, x
   STA $0200, x
   INX
   CPX #$10
   BNE LoadSpritesLoop
 
-; todo: replace LoadBackground
-; After some struggling, this was the first background loading subroutine which worked.
-; I don't fully understand it yet and will replace/refactor to fit my style.
 LoadBackground:
-  LDA $2002             ; read PPU status to reset the high/low latch
+  LDA PPU_STATUS
+  ; write $2000 address to $2006
   LDA #$20
-  STA $2006             ; write the high byte of $2000 address
-  LDA #$00
-  STA $2006             ; write the low byte of $2000 address;
-
-  LDA #$00
-  STA pointerLo       ; put the low byte of the address of background into pointer
-  LDA #HIGH(background)
-  STA pointerHi       ; put the high byte of the address into pointer
-  
-  LDX #$00            ; start at pointer + 0
-  LDY #$00
-OutsideLoop:
-  
-InsideLoop:
-  LDA [pointerLo], y  ; copy one background byte from address in pointer plus Y
-  STA $2007           ; this runs 256 * 4 times
-  
-  INY                 ; inside loop counter
-  CPY #$00
-  BNE InsideLoop      ; run the inside loop 256 times before continuing down
-  
-  INC pointerHi       ; low byte went 0 to 256, so high byte needs to be changed now
-  
-  INX
-  CPX #$04
-  BNE OutsideLoop     ; run the outside loop 256 times before continuing down
-
-              
-LoadAttribute:
-; send attribute data to PPU
-; $23C0
-  LDA $2002
-  LDA #$23
   STA $2006
-  LDA #$C0
+  LDA #$00
   STA $2006
+
+  LDA #>Background
+  STA pointerLow
+  LDA #<Background 
+  STA pointerHigh
+
+; Need to copy 930 (0x3A2) bytes to PPU
+; 3*255(0xFF) with 165 (0xA5) remainder
   LDX #$00
-LoadAttributeLoop:
-  LDA attribute, x
-  STA $2007
+@OUTER_LOOP:
+  CPX #$03
+  BCS @LAST_LOOP
+  LDY #$00
+@INNER_LOOP:
+  LDA (pointerHigh), Y
+  STA PPU_VRAM_IO 
+  INY
+  BNE @INNER_LOOP
   INX
-  CPX #$08
-  BNE LoadAttributeLoop
+  INC pointerLow
+  JMP @OUTER_LOOP
+@LAST_LOOP:
+  LDA (pointerHigh), Y
+  STA PPU_VRAM_IO 
+  INY
+  CPY #$A5
+  BCC @LAST_LOOP
 
-EnableNMI:
-  LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
-  STA $2000
+SetupPpu:
+  JSR PPU_SETUP
+  JSR EnableSprites
+
+GameLoop:
+  JMP GameLoop
+
+PPU_SETUP:
+  ;LDA #%00000000
+  ;ORA CRTL1_NMI_ENABLED
+  ;;ORA CRTL1_SPRITES_8x16
+  ;ORA CRTL1_BG_PT_ADDR_1
+  ;;ORA CRTL1_SP_PT_ADDR_1
+  ;;ORA CRTL1_VRAM_INC
+  ;;ORA CRTL1_BASE_NAMETABLE_2000
+  ;;ORA CRTL1_BASE_NAMETABLE_2400
+  ;;ORA CRTL1_BASE_NAMETABLE_2800
+  ;;ORA CRTL1_BASE_NAMETABLE_2C00
+  LDA #%10010000
+  STA PPU_CTRL1 
+  RTS
 
 EnableSprites:
-  ;; Set up the PPU
-  ;; PPUMASK ($2001)
-  ;;
-  ;; 76543210
-  ;; ||||||||
-  ;; |||||||+- Grayscale (0: normal color; 1: AND all palette entries
-  ;; |||||||   with 0x30, effectively producing a monochrome display;
-  ;; |||||||   note that colour emphasis STILL works when this is on!)
-  ;; ||||||+-- Disable background clipping in leftmost 8 pixels of screen
-  ;; |||||+--- Disable sprite clipping in leftmost 8 pixels of screen
-  ;; ||||+---- Enable background rendering
-  ;; |||+----- Enable sprite rendering
-  ;; ||+------ Intensify reds (and darken other colors)
-  ;; |+------- Intensify greens (and darken other colors)
-  ;; +-------- Intensify blues (and darken other colors)
+  ;LDA #%00000000
+  ;;ORA CRTL2_GRAYSCALE
+  ;ORA CRTL2_DISABLE_LEFT_BG_CLIP
+  ;ORA CRTL2_DISABLE_LEFT_SPR_CLIP
+  ;ORA CRTL2_BACKGROUND_RENDER
+  ;ORA CRTL2_SPRITE_RENDER
+  ;;ORA CRTL2_INTENSE_RED
+  ;;ORA CRTL2_INTENSE_GREEN
+  ;;ORA CRTL2_INTENSE_BLUE
+  ;;LDA #%00011110
+  ;STA PPU_CTRL2 
+  ;RTS
   LDA #%00011110
-  STA $2001
+  STA PPU_CTRL2
 
+PpuCleanup:
 
-; Once main program execution hits here, it loops infinitely.
-; VBlank throws NMI interrupt when the screen is done drawing
-; which causes NMI section to run (60x per sec).  All program 
-; logic is currently in NMI, which is not best practice, but 
-; it works at the moment.
-Forever:
-  JMP Forever
-
-NMI:
+VBlank:
   JSR HandleControllerInput
   JSR SetSpritePositions
   JSR CopySpritesToPpu
+  JSR PPU_SETUP
+  JSR EnableSprites
 
   ;;This is the PPU clean up section, so rendering the next frame starts properly.
-  LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
-  STA $2000
-  LDA #%00011110   ; enable sprites, enable background, no clipping on left side
-  STA $2001
+  ;LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
+  ;STA PPU_CTRL1 
+  ;LDA #%00011110   ; enable sprites, enable background, no clipping on left side
+  ;STA PPU_CTRL2
   LDA #$00        ;;tell the ppu there is no background scrolling
-  STA $2005
-  STA $2005
+  STA PPU_VRAM_ADDR1 
+  STA PPU_VRAM_ADDR1 
 
   RTI
 
-  .bank 1
-  .org $E000
+Dummy:
+  RTI
 
-background:
-  .db $10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10
-  .db $10,$40,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$41,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
-  .db $10,$43,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$42,$10
-  .db $10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10
+Background:
+  .byte $10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10
+  .byte $10,$40,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$31,$41,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$30,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$32,$10
+  .byte $10,$43,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$33,$42,$10
+  .byte $10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10,$10
 
 attribute:
-  .db $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
-  .db $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
-  .db $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
-  .db $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
-  .db $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
-  .db $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
-  .db $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
-  .db $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
-  .db $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
-  .db $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
-  .db $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
-  .db $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
-  .db $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
-  .db $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
-  .db $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
+  .byte $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
+  .byte $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
+  .byte $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
+  .byte $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
+  .byte $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
+  .byte $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
+  .byte $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
+  .byte $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
+  .byte $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
+  .byte $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
+  .byte $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
+  .byte $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
+  .byte $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
+  .byte $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
+  .byte $55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55,$55
 
-palettes:
+Palettes:
   ; each palette consists of 4 bytes, starting with $0F (transparent background)
   ; colors are defined in the PPU and are looked up by bytes. No RGB here.
 
   ; background
-  .db $0F,$2D,$3D,$30,$0F,$2D,$3D,$30,$0F,$2D,$3D,$30,$0F,$2D,$3D,$30
+  .byte $0F,$2D,$3D,$30,$0F,$2D,$3D,$30,$0F,$2D,$3D,$30,$0F,$2D,$3D,$30
   ; sprite
-  .db $0F,$1C,$15,$19,$0F,$02,$38,$12,$0F,$1C,$15,$16,$0F,$02,$38,$3C
+  .byte $0F,$1C,$15,$19,$0F,$02,$38,$12,$0F,$1C,$15,$16,$0F,$02,$38,$3C
 
-sprites:
+Sprites:
   ;   vert tile attr horiz
-  .db $80, $00, $00, $80 ; T sprite
-  .db $80, $01, $01, $84 ; o sprite
-  .db $80, $02, $02, $90 ; m sprite
+  .byte $80, $00, $00, $80 ; T sprite
+  .byte $80, $01, $01, $84 ; o sprite
+  .byte $80, $02, $02, $90 ; m sprite
 
-  ;.db $24,$24,$24,$24, $47,$47,$24,$24 ,$47,$47,$47,$47, $47,$47,$24,$24 ,$24,$24,$24,$24 ,$24,$24,$24,$24, $24,$24,$24,$24, $55,$56,$24,$24  ;;brick bottoms
+.segment "VECTORS"
+.word VBlank, Reset, Dummy
 
-  ; Register interrupt handlers
-  .org $FFFA
-  .dw NMI
-  .dw RESET
-  .dw 0
-
-  ; Bank 2 for sprites
-  .bank 2
-  .org $0000
-  .incbin "simple.chr"
+.segment "CHRROM"
+.incbin "simple.chr"
